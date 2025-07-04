4571b8ea9c326402d9e19e9cda69ed62da48389d
Anton Syzov <aantonssizov@gmail.com>
2025-07-04 18:17:51 -0400

    Week 01 Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int magicNumber;
+        int guess;
+        int attempts;
+        string keepPlaying = "yes";
+
+        while (keepPlaying == "yes")
+        {
+            magicNumber = new Random().Next(1, 101);
+            attempts = 0;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                attempts++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it! It took " + attempts + " attempts.");
+                    Console.Write("Do you want to play again? (yes/no) ");
+                    keepPlaying = Console.ReadLine().ToLower();
+                }
+            } while (guess != magicNumber);
+        }
     }
 }
