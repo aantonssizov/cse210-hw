f50e8c80d23252c1aa33d6ab6b803e4fcc96cee4
Anton Syzov <aantonssizov@gmail.com>
2025-07-10 18:19:28 -0400

    W02 Learning Activity: Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Resume myResume = new()
+        {
+            _name = "Anton Syzov"
+        };
+
+        Job job1 = new()
+        {
+            _jobTitle = "Software Engineer",
+            _company = "Netflix",
+            _startYear = 2013,
+            _endYear = 2015
+        };
+
+        Job job2 = new()
+        {
+            _jobTitle = "DevOps Engineer",
+            _company = "Highscore Technologies",
+            _startYear = 2017,
+            _endYear = 2018
+        };
+
+        myResume._jobs.AddRange([job1, job2]);
+
+        myResume.Display();
     }
 }
