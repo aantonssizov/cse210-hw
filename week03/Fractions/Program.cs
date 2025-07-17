9e839e9defb5668f9237f0ff6174043f519073cb
Anton Syzov <aantonssizov@gmail.com>
2025-07-17 17:09:34 -0400

    W03 Learning Activity: Encapsulation

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction firstFraction = new();
+        Fraction secondFraction = new(4);
+        Fraction thirdFraction = new(3, 4);
+
+        Console.WriteLine(firstFraction.GetFractionString());
+
+        secondFraction.SetTop(2);
+        Console.WriteLine(secondFraction.GetFractionString());
+
+        Console.WriteLine(thirdFraction.GetDecimalValue());
     }
 }
